507d0f552bf1f7dd8a21098bf717728837266206
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2019-02-23 17:29:54 +0000

    lint:ObsoleteLintCustomCheck

---
+++ Lint/ObsoleteLintCustomCheck/LintApiNewer/src/main/kotlin/net/twisterrob/lints/lint/LintApiNewerRegistry.kt
@@ -0,0 +1,9 @@
+package net.twisterrob.lints.lint
+
+import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.detector.api.*
+
+class LintApiNewerRegistry : IssueRegistry() {
+
+	override val issues: List<Issue> = listOf()
+}
