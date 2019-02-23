b4ef7b25e1b8bc3255ddd8e88ed4be262741a0ce
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2019-02-23 03:28:38 +0000

    lint:UniqueConstants fix compilation warning and name

--- Correctness/UniqueConstants/src/main/java/net/twisterrob/lints/correctness/UniqueConstantsViolation.java
+++ Correctness/UniqueConstants/src/main/java/net/twisterrob/lints/correctness/UniqueConstantsViolation.java
@@ -1,7 +1,11 @@
 package net.twisterrob.lints.correctness;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import androidx.annotation.IntDef;
 
+@Retention(RetentionPolicy.SOURCE)
 @IntDef({1, 2, 1})
-@interface NonUniqueConstants {
+@interface UniqueConstantsViolation {
 }
