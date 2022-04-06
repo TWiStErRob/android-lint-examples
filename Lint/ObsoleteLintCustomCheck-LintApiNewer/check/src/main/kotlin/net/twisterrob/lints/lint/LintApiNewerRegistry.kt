24d47700bc434919a48490374c6b90bcb81800f2
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-04-06 23:07:29 +0100

    lint:ObsoleteLintCustomCheck add vendor info to satisfy requirements.

--- Lint/ObsoleteLintCustomCheck-LintApiNewer/check/src/main/kotlin/net/twisterrob/lints/lint/LintApiNewerRegistry.kt
+++ Lint/ObsoleteLintCustomCheck-LintApiNewer/check/src/main/kotlin/net/twisterrob/lints/lint/LintApiNewerRegistry.kt
@@ -1,9 +1,17 @@
 package net.twisterrob.lints.lint
 
 import com.android.tools.lint.client.api.IssueRegistry
-import com.android.tools.lint.detector.api.*
+import com.android.tools.lint.client.api.Vendor
+import com.android.tools.lint.detector.api.Issue
 
 class LintApiNewerRegistry : IssueRegistry() {
+
+	override val vendor: Vendor = Vendor(
+		vendorName = "TWiStErRob",
+		identifier = "android-lint-examples",
+		feedbackUrl = "https://github.com/TWiStErRob/android-lint-examples/issues",
+		contact = "https://github.com/TWiStErRob/android-lint-examples"
+	)
 
 	override val issues: List<Issue> = emptyList()
 
