ae01407d8c78a9ae4be0086c79be9759f581de0b
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-04-06 23:07:29 +0100

    lint:ObsoleteLintCustomCheck add vendor info to satisfy requirements.

--- Lint/ObsoleteLintCustomCheck-UnlistableRegistry/check/src/main/kotlin/net/twisterrob/lints/lint/UnlistableRegistry.kt
+++ Lint/ObsoleteLintCustomCheck-UnlistableRegistry/check/src/main/kotlin/net/twisterrob/lints/lint/UnlistableRegistry.kt
@@ -1,9 +1,18 @@
 package net.twisterrob.lints.lint
 
 import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.client.api.Vendor
 import com.android.tools.lint.detector.api.Issue
 
 class UnlistableRegistry : IssueRegistry() {
 
-	override val issues: List<Issue> get() = error("Cannot load issues")
+	override val vendor: Vendor = Vendor(
+		vendorName = "TWiStErRob",
+		identifier = "android-lint-examples",
+		feedbackUrl = "https://github.com/TWiStErRob/android-lint-examples/issues",
+		contact = "https://github.com/TWiStErRob/android-lint-examples"
+	)
+
+	override val issues: List<Issue>
+		get() = error("Cannot load issues")
 }
