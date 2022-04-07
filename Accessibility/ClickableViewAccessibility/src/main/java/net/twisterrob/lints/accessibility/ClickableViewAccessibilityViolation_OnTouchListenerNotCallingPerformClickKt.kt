e35af10c05599c2dbfd4b42f6578510d920081ee
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-04-07 02:09:17 +0100

    lint:ClickableViewAccessibility fully works now

--- Accessibility/ClickableViewAccessibility/src/main/java/net/twisterrob/lints/accessibility/ClickableViewAccessibilityViolation_OnTouchListenerNotCallingPerformClickKt.kt
+++ Accessibility/ClickableViewAccessibility/src/main/java/net/twisterrob/lints/accessibility/ClickableViewAccessibilityViolation_OnTouchListenerNotCallingPerformClickKt.kt
@@ -3,7 +3,6 @@ package net.twisterrob.lints.accessibility
 import android.view.View
 
 fun onTouchListenerNotCallingPerformClick(view: View) {
-	// REPORT Kotlin lambdas and SAMs are not detected
 	view.setOnTouchListener { _, _ -> true }
 	view.setOnTouchListener(View.OnTouchListener { _, _ -> true })
 }
