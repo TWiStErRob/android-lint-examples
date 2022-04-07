18087f7b4e531341722ad832fd9caa98b1d22d11
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-04-07 01:42:23 +0100

    lint:StringShouldBeInt small improvements

--- Correctness/StringShouldBeInt-kts/build.gradle.kts
+++ Correctness/StringShouldBeInt-kts/build.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+	// REPORT reverting this to manual fun breaks the detector
+	// Force Kotlin DSL fun android(block: com.android.build.gradle.LibraryExtension.() -> Unit) = configure(block)
+	id("com.android.library")
+}
 apply(from = rootProject.file("gradle/module-check-lib.gradle"))
 val expectedViolationCount: groovy.lang.Closure<Any?> by project.extra
 
@@ -6,6 +11,7 @@ expectedViolationCount(2 /*compile*/ + 1 /*min*/ + 1 /*target*/)
 
 // Need to disable execution path, otherwise Gradle configuration phase would fail.
 if (@Suppress("ConstantConditionIf") false) {
+	// REPORT doesn't work with configure<com.android.build.gradle.LibraryExtension> { compileSdkVersion("29") }
 	android {
 		compileSdkVersion("29")
 		compileSdkVersion = "29"
@@ -19,8 +25,3 @@ if (@Suppress("ConstantConditionIf") false) {
 		}
 	}
 }
-
-
-// Kotlin DSL doesn't generate this, so be explicit. It is necessary otherwise GradeDetector doesn't recognize
-// REPORT doesn't work with configure<com.android.build.gradle.LibraryExtension> { compileSdkVersion("29") }
-fun android(block: com.android.build.gradle.LibraryExtension.() -> Unit) = configure(block)
