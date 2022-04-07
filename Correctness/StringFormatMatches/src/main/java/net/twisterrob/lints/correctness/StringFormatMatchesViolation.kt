2f19ce6f0e0a74bbd1239fb03861cc0b3e0a849c
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-04-07 02:24:54 +0100

    lint:StringFormatMatches now works in Kotlin

--- Correctness/StringFormatMatches/src/main/java/net/twisterrob/lints/correctness/StringFormatMatchesViolation.kt
+++ Correctness/StringFormatMatches/src/main/java/net/twisterrob/lints/correctness/StringFormatMatchesViolation.kt
@@ -3,7 +3,6 @@ package net.twisterrob.lints.correctness
 import android.content.res.Resources
 import net.twisterrob.lints.correctness.StringFormatMatches.R
 
-// REPORT Kotlin not included in results
 fun format(res: Resources) = listOf(
 	res.getString(R.string.invalid, "text"),
 	res.getString(R.string.suspicious, 42),
